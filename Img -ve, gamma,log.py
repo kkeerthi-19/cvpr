35e453536f3875615751c4074d9c140904573444
kkeerthi-19 <siri.keerthi19@gmail.com>
2025-05-15 10:27:42 +0530

    Img -ve, gamma,log.py

--- Img -ve, gamma,log.py
+++ Img -ve, gamma,log.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 img=cv2.imread('img.jpg',0)
 neg=255-img
+logt=np.unit8(45*np.log1p(img))
 gamma=np.array((255*img/255)**2.2, dtype='uint8'))
 cv2.imshow('img neg', neg)
 cv2.imshow('gamma', gamma)
